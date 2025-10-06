0e4f47e7c97405efc567e585bf2937bc8be22bf4
RomanNep <neproman@gmail.com>
2025-10-06 12:03:48 +0400

    db connect

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,12 +1,12 @@
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
+import * as dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 export const databaseConfig: SequelizeModuleOptions = {
   dialect: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'username',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_DATABASE || 'the_last_of_guss',
+  uri: process.env.DB_URI || 'none',
   models: [__dirname + '/../models/*.model.ts'],
   autoLoadModels: true,
   synchronize: false, // We'll handle initialization manually
